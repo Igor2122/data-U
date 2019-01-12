cf77b457296bb61e795deaba1265288c7f532655
Igor <igor_pavlenko@yahoo.com>
2019-01-12 20:15:09 +0100

    playing around with sass

---
+++ practice/sass/app.js
@@ -0,0 +1,63 @@
+// // const sayHello = (name = 'Guest', sign='No sign') => {
+// //     // name = name || 'Guest';
+// //     print('Hello ' + name + ', ' + sign + ' you are wellcome here');
+// // }
+
+// // function print (par) {
+// //     console.log(par);
+// // }
+
+// // const checker = (num) => {
+// //     return (num > 0) ?  'passed' : 'fail';
+// // }
+
+// // let checked = checker(5);
+// // print(checked);
+
+// // const reverse = (str) => {
+    
+// // }
+
+// // let string = 'automibile';
+// // let i = 5;
+// // function recursiveEl (par) {
+// //     if(par === 1){
+// //         return par
+// //     }
+// //     return par * recursiveEl( par - 1 );
+// //     //  
+// // }
+// // // console.log(recursiveEl(i));
+
+// // const vozved = (a,b) => {
+// //     let res = 1;
+// //     for(let i = 0; i < b; i++){
+// //         res*=a;
+// //     }
+// //     return res;
+// // }
+
+// // console.log(vozved(2,3));
+
+
+// let sentence = ['I ', 'am ', 'Igor'];
+
+// const writeSentece = (arr) => {
+
+//     function alertsomthing() {
+//         alert('hello');
+//     }
+
+//     return alertsomthing;
+// }
+
+// // alertsomthing();
+
+// writeSentece(sentence);
+
+
+
+
+
+
+
