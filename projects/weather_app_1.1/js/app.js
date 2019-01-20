18af539da49bd1fac44af8a6da87e1b2f7205f30
Igor <igor_pavlenko@yahoo.com>
2019-01-20 16:23:40 +0100

    rendering countries from json object for the country selector modal window

--- projects/weather_app_1.1/js/app.js
+++ projects/weather_app_1.1/js/app.js
@@ -1,33 +1,35 @@
-// instant weather object 
+// // instant weather object 
 
-const weather = new Weatehr();
+// const weather = new Weatehr();
 const ui = new Ui();
 
-
-
-let userCityChooser = ui.getData();
-weather.changeLocation('703448');
-
-
-const getWeatehr = () => {
-    weather.getWeatehr()
-    .then(res => {
-        console.log(res);
-        ui.dropDownItems(res);
-    })
-    .catch(err => console.log(err));
-}
-
-document.addEventListener('DOMContentLoaded', getWeatehr)
+ui.coutriesGenerator();
 
 
 
-// Connect to API on the DOM load 
-
-// ui.citySearch();
-ui.getInputValue();
+// let userCityChooser = ui.getData();
+// weather.changeLocation('703448');
 
 
+// const getWeatehr = () => {
+//     weather.getWeatehr()
+//     .then(res => {
+//         console.log(res);
+//         ui.dropDownItems(res);
+//     })
+//     .catch(err => console.log(err));
+// }
 
-// let cityId = ui.getData();
-// console.log(cityId);
+// document.addEventListener('DOMContentLoaded', getWeatehr)
+
+
+
+// // Connect to API on the DOM load 
+
+// // ui.citySearch();
+// ui.getInputValue();
+
+
+
+// // let cityId = ui.getData();
+// // console.log(cityId);
--- projects/weather_app_1.1/js/ui.js
+++ projects/weather_app_1.1/js/ui.js
@@ -4,14 +4,13 @@ class Ui {
     }
 
     getData(val) {
-        const countryDropDown = document.querySelector('dropdown-menu');
         const cities = fetch("js/test.json")
             .then(function (resp) {
                 return resp.json();
             })
             .then(function (data) {
                 data.map(res => {
-                    if(res['name'] == val){
+                    if (res['name'] == val) {
                         console.log(res['id']);
                         let id = res['id'];
                         return id;
@@ -20,13 +19,51 @@ class Ui {
             })
     }
 
-    // helper functions 
+    coutriesGenerator(val) {
+        let countriesDropDown = document.querySelector('.country-selector');
+        let countriesList = [];
 
-    convertKelnToCelcius (a ){
-        return  a - 273.15;
+        const cities = fetch("js/test.json")
+            .then(function (resp) {
+                return resp.json();
+            })
+            .then(function (data) {
+
+                data.map(res => {
+                    // console.log(res);
+
+                    countriesList.push(res['country']);
+                    // console.log(countriesList);
+                    return countriesList;
+
+
+
+                })
+                const unitqueCoutries = [...new Set(countriesList)];
+                // console.log(unitqueCoutries);
+                for (let i of unitqueCoutries) {
+                    console.log(i);
+                    let option = document.createElement('option');
+                    option.textContent = i;
+                    countriesDropDown.appendChild(option);
+                }
+            })
+
     }
 
-    dropDownItems (val) {
+    // helper functions 
+
+    convertKelnToCelcius(a) {
+        return a - 273.15;
+    }
+
+    weatherIconSelector(val) {
+        let icons = {
+            sunny: sunny.png,
+        }
+    }
+
+    dropDownItems(val) {
         const temperature = document.querySelector('#cur-temp');
         temperature.textContent = `Temperature: ${parseInt(this.convertKelnToCelcius(val['main']['temp']))}`;
 
@@ -39,7 +76,7 @@ class Ui {
 
     getInputValue(e) {
         const input = document.querySelector('#city');
-        
+
         input.addEventListener('keyup', (e) => {
             let userInput = e.target.value;
             if (userInput !== '') {
