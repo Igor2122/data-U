86faf0e3e43d56a42ba516617aa43777d2293966
Igor <igor_pavlenko@yahoo.com>
2019-01-28 16:57:38 +0100

    added the movies to the movie class

---
+++ practice/library/app.js
@@ -0,0 +1,24 @@
+const allProducts = [];
+
+const newProduct = (val1, val2) => {
+    const groceryListObj = {
+        product: ' ',
+        ammount: ' '
+    }
+    groceryListObj.product = val1;
+    groceryListObj.ammount = val2;
+    allProducts.push(groceryListObj);
+}
+
+newProduct('product1', 'product1');
+newProduct('product2', 'product2');
+
+// console.log(groceryListObj);
+
+
+
+
+// console.log(allProducts);
+
+const BookObj = new Book();
+BookObj.renderBook();
---
+++ practice/library/book.js
@@ -0,0 +1,74 @@
+class Book {
+    constructor(title, director, pages, publishEar, takenHome, available) {
+        title = this.title;
+        director = this.director;
+        pages = this.pages;
+        publishEar = this.publishEar;
+        takenHome = this.takenHome;
+        available = this.available;
+    }
+
+    bookList() {
+        const bookList = [{
+            title: 'Pulp Fiction',
+            director: 'Quentin Tarantino',
+            image: 'https://static.rogerebert.com/uploads/movie/movie_poster/pulp-fiction-1994/large_dM2w364MScsjFf8pfMbaWUcWrR.jpg',
+            genre: ['Action', 'Crime', 'Thriller'],
+            rated: 'R',
+            available: true,
+            takenHome: false
+        }]
+        return bookList;
+    }
+
+    // helper functions 
+
+    outputArray(val) {
+        let res = '';
+        for (let i of val) {
+            res += `${i}, `;
+        }
+        return res;
+    }
+
+    takeHome(val, replyTrue, replyFalse) {
+
+        return val ? replyTrue : replyFalse;
+
+    }
+
+    renderBook() {
+        let books = this.bookList();
+
+        for (let book of books) {
+
+            let bookOutput = document.createElement('div');
+            bookOutput.innerHTML = `<div class="card" style="width: 18rem;">
+            <img src=${book.image} class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${book.title}</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+            <ul class="list-group list-group-flush">
+            <li class="list-group-item">Director: ${book.director}</li>
+            <li class="list-group-item">Gentre: ${this.outputArray(book.genre)}</li>
+            <li class="list-group-item">Rated: ${book.rated}</li>
+            
+            <li class="list-group-item">Available: ${this.takeHome(book.available, 'Knock Yourself out!', 'Read Here Only')}</li>
+            <li class="list-group-item">Taken Home: ${this.takeHome(book.takenHome, 'OK', 'No, Sorry')}</li>
+            </ul>
+            <div class="card-body">
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+            </div>
+            </div>`;
+
+            const bookParent = document.querySelector('.books');
+            bookParent.appendChild(bookOutput);
+
+        }
+    }
+
+
+
+}
