215ed27f51e893eea36ccc446543c0a7c6b35f48
Igor <igor_pavlenko@yahoo.com>
2019-01-29 12:51:11 +0100

    refactoring on chatbots app

--- practice/chatbots/app.js
+++ practice/chatbots/app.js
@@ -1,27 +1,23 @@
-const chatBox = document.querySelector('.chatbox');
-
-
-
-const createElement = (val) => {
-    let chat = document.createElement('li');
-    chat.innerHTML = `
-    <li class="list-group-item d-flex justify-content-between align-items-center">
-        <div class="align-self-end">
-        <img class="w-25 img" src="./img/${val.name.toLowerCase()}.png" alt="local" >
-            <span class="badge badge-primary badge-pill">${val.text}</span>
-        </div>
-    </li>
-    `
-    chatBox.appendChild(chat);
-    
-}
-for (let i of conversation) {
-        createElement(i);
-}
 document.addEventListener('DOMContentLoaded', () => {
+
+
+const chatBox = document.querySelector('.chatbox');
+const createElement = (val) => {
+    let li = document.createElement('li');
+    li.className = 'list-group-item d-flex justify-content-between align-items-center';
+    li.innerHTML = `
+        <img class="w-25 img" src="./img/${val.name.toLowerCase()}.png" alt="local" >
+        <span class="badge badge-primary badge-pill">${val.text}</span>
+    `
+    return li;
     
+}
+for (let chat of conversation) {
+        createElement(chat);
+        chatBox.appendChild(createElement(chat));
+        
+}
     let allImages = document.querySelectorAll('.img');
-    console.log(allImages);
     for(let img of allImages){
         img.addEventListener('click', () => {
             alert('don\'t click on my head ');
